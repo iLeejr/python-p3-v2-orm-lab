1c087de999d14e7c40f819076e21d80382ce78f6
Kaleb Malone <kalebmalone7@gmail.com>
2024-11-06 13:20:42 -0800

    completed lab

--- lib/employee.py
+++ lib/employee.py
@@ -1,11 +1,10 @@
-# lib/employee.py
 from __init__ import CURSOR, CONN
 from department import Department
 
 class Employee:
 
-    # Dictionary of objects saved to the database.
     all = {}
+
 
     def __init__(self, name, job_title, department_id, id=None):
         self.id = id
@@ -118,10 +117,8 @@ class Employee:
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
 
-        # Delete the dictionary entry using id as the key
         del type(self).all[self.id]
 
-        # Set the id to None
         self.id = None
 
     @classmethod
@@ -135,15 +132,12 @@ class Employee:
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
 
-        # Check the dictionary for  existing instance using the row's primary key
         employee = cls.all.get(row[0])
         if employee:
-            # ensure attributes match row values in case local instance was modified
             employee.name = row[1]
             employee.job_title = row[2]
             employee.department_id = row[3]
         else:
-            # not in dictionary, create new instance and add to dictionary
             employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
@@ -187,4 +181,13 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
+        from review import Review
+        sql = """
+            SELECT * FROM reviews
+            WHERE employee_id is?
+        """
+        CURSOR.execute(sql, (self.id,),)
+
+        rows = CURSOR.fetchall()
+        return [Review.instance_from_db(row) for row in rows]
         pass
